94b056aa381ca3fa0edef02e9e4a1735b62bccfe
ReicomA <seokbong60@gmail.com>
2021-01-08 12:51:39 +0900

    change bakhwa navi bar

--- app/src/components/bakhwa/Navbar.tsx
+++ app/src/components/bakhwa/Navbar.tsx
@@ -18,7 +18,7 @@ const Navbar: React.FC<{
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container-fluid">
-                <a className="navbar-brand" href="/bakhwaproject"><NavTitle>白化 Project</NavTitle></a>
+                <a className="navbar-brand" href="/bakhwaproject"><NavTitle>白花 Project</NavTitle></a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
