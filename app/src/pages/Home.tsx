c0f825c2c582a3b1de5026b5c6d39fa0fb7ff1fe
ReicomA <seokbong60@gmail.com>
2021-03-12 22:04:22 +0900

    03/12

--- app/src/pages/Home.tsx
+++ app/src/pages/Home.tsx
@@ -4,9 +4,13 @@ import Helmet from 'react-helmet';
 import {IsPC, IsMobile} from "../configs/ResponseWebRange";
 import MainTitleImg from "../img/SweetcaseTitleLogo.gif";
 
+import config from "../config.json";
+import '../fonts/fonts.css';
+
 const Main = styled.div`
     width: 100%;
     height: 100%;
+    color: ${config.colors['main-color']}
 `
 
 const PageHome: React.FC = () => {
@@ -20,7 +24,14 @@ const PageHome: React.FC = () => {
                 <div>
                     <TitleImgInPC src={MainTitleImg}/>
                     <MainLayoutInPC>
-                        <h1>PC</h1>
+                        <BakhwaProjectLayoutInPC>
+                            <BakhwaProjectTitleText>白花 PROJECT</BakhwaProjectTitleText>
+                            <MainMiddleLine />
+                        </BakhwaProjectLayoutInPC>
+                        <BlackLineProjectLayoutInPC>
+                            <BlackLineProjectTitle>BLACKLINE</BlackLineProjectTitle>
+                            <MainMiddleLine />
+                        </BlackLineProjectLayoutInPC>
                     </MainLayoutInPC>
                 </div>
                 
@@ -30,14 +41,36 @@ const PageHome: React.FC = () => {
                 <div>
                     <TitleImgInMobile src={MainTitleImg}/>
                     <MainLayoutInMobile>
-                        <h1>Mobile</h1>
+                        <BakhwaProjectLayoutInMobile>
+                            <BakhwaProjectTitleText>白花 PROJECT</BakhwaProjectTitleText>
+                            <MainMiddleLine />
+                        </BakhwaProjectLayoutInMobile>
+                        <BlackLineProjectLayoutInMobile>
+                            <BlackLineProjectTitle>BLACKLINE</BlackLineProjectTitle>
+                            <MainMiddleLine />
+                        </BlackLineProjectLayoutInMobile>
                     </MainLayoutInMobile>
-                    
                 </div>
             }
         </Main>
     );
 }
+
+
+// ALL PC And Mobile
+const BlackLineProjectTitle = styled.h1`
+    font-family: 'Android_101';
+`
+const BakhwaProjectTitleText = styled.h1`
+    font-family: 'Nanum Myeongjo', serif;
+`
+const MainMiddleLine = styled.div`
+    margin: 10px;
+    height: 1px;
+    width: 90%;
+    background-color: ${config.colors['shallow-color']};
+`
+
 
 // PC EDITION
 const TitleImgInPC = styled.img`
@@ -52,8 +85,13 @@ const MainLayoutInPC = styled.div`
     margin-top: 300px;
     margin-left: 30px;
     margin-right: 30px;
+    width: 90%;
 
     position: absolute;
+`
+const BakhwaProjectLayoutInPC = styled.div`
+`
+const BlackLineProjectLayoutInPC = styled.div`
 `
 
 // MOBILE EDITION
@@ -69,9 +107,13 @@ const MainLayoutInMobile = styled.div`
     margin-top: 200px;
     margin-left: 30px;
     margin-right: 30px;
+    width: 90%;
 
     position: absolute;
-    
+`
+const BakhwaProjectLayoutInMobile = styled.div`
+`
+const BlackLineProjectLayoutInMobile = styled.div`
 `
 
 export default PageHome;
