894264e69b3bf95a4d8969999acbf2fede069da8
ReicomA <seokbong60@gmail.com>
2021-03-14 08:43:44 +0900

    add main page by pc

--- app/src/components/navbar/MainNavbar.tsx
+++ app/src/components/navbar/MainNavbar.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
 import {IsPC, IsMobile} from '../../configs/ResponseWebRange';
+import '../../fonts/fonts.css';
+
+import SweetcaseTitle from '../../img/sweetcasetitle.gif';
+import webColor from '../../config.json';
 
 const MainNavbar: React.FC = () => {
     return (
@@ -8,12 +12,18 @@ const MainNavbar: React.FC = () => {
             { 
                 IsPC() && 
                 <div>
+                    <MainNavbarStyle>
+                        <ItemInPC><img src={SweetcaseTitle} height="15px"/></ItemInPC>
+                        <ItemInPC>introduce</ItemInPC>
+
+                        <CobaltoLinkInPC>cobalto</CobaltoLinkInPC>
+                        <BakhwaLinkInPC>白花 PROJECT</BakhwaLinkInPC>
+                    </MainNavbarStyle>
                 </div>
             }
             {
                 IsMobile() &&
                 <div>
-                    <MobileMainTitle>SWEETCASE</MobileMainTitle>
                 </div>
             }
 
@@ -23,17 +33,38 @@ const MainNavbar: React.FC = () => {
 export default MainNavbar;
 
 const MainNavbarStyle = styled.nav`
-    left: 0;
-    right: 0;
-    background-color: #0E6AC7;
+    background-color: white;
     width: 100%;
     height: 50px;
+    border-bottom: 1px solid gray;
 `
 
 // PC
-
-// Mobile
-const MobileMainTitle = styled.p`
-    font-size: 30px;
-    text-align: center;
+const ItemInPC = styled.div`
+    float: left;
+    margin-left: 30px;
+    padding-top: 10px;
+    font-family: Android_101;
+    font-size: 25px;
+    cursor: pointer;
 `
+const BakhwaLinkInPC = styled.div`
+    float: right;
+    margin-right: 30px;
+    padding-top: 10px;
+    font-family: 'Nanum Myeongjo', serif;
+    font-size: 25px;
+    color: ${webColor.colors.bakhwa.main};
+    cursor: pointer;
+
+`
+const CobaltoLinkInPC = styled.div`
+    float: right;
+    margin-right: 30px;
+    padding-top: 10px;
+    font-family: Android_101;
+    font-size: 25px;
+    color: ${webColor.colors.cobalto.main};
+    cursor: pointer;
+`
+// Mobile
--- app/src/pages/Home.tsx
+++ app/src/pages/Home.tsx
@@ -2,15 +2,19 @@ import React from 'react';
 import styled from 'styled-components';
 import Helmet from 'react-helmet';
 import {IsPC, IsMobile} from "../configs/ResponseWebRange";
-import MainTitleImg from "../img/SweetcaseTitleLogo.gif";
 
-import config from "../config.json";
+import MainNavbar from "../components/navbar/MainNavbar";
+
 import '../fonts/fonts.css';
+import webColor from '../config.json';
+
+import MainDisplay from "../img/main-display.gif";
+
+import BakhwaExampleNews from "../img/examples/bakhwa-main-title.png";
+import CobaltoExampleNews from "../img/examples/cobalto-main-title.png";
 
 const Main = styled.div`
     width: 100%;
-    height: 100%;
-    color: ${config.colors['main-color']}
 `
 
 const PageHome: React.FC = () => {
@@ -22,58 +26,118 @@ const PageHome: React.FC = () => {
             {
                 IsPC() &&
                 <div>
-                    <TitleImgInPC src={MainTitleImg}/>
-                    <MainLayoutInPC>
-                    </MainLayoutInPC>
+
+                    <MainNavbar />
+                    <MainDisplayInPC src={MainDisplay} />
+
+                    <BakhwaWallLine>白花 PROJECT</BakhwaWallLine>
+                    <BakhwaNewsLayoutInPC>
+                        <BakhwaNewsTitleText>News</BakhwaNewsTitleText>
+                        <BakhwaNewsImg src={BakhwaExampleNews} />
+                    </BakhwaNewsLayoutInPC>
+
+                    <CobaltoWallLine>cobalto</CobaltoWallLine>
+                    <CobaltoNewsLayoutInPC>
+                        <CobaltoNewsTitleText>News</CobaltoNewsTitleText>
+                        <CobaltoNewsImg src={CobaltoExampleNews} />
+                    </CobaltoNewsLayoutInPC>
+
+                    <CommunicationWallLine>communications</CommunicationWallLine>
+
                 </div>
-                
             }
             {
                 IsMobile() &&
                 <div>
-                    <TitleImgInMobile src={MainTitleImg}/>
-                    <MainLayoutInMobile>
-                    </MainLayoutInMobile>
                 </div>
             }
         </Main>
     );
 }
 
-
-// PC EDITION
-const TitleImgInPC = styled.img`
-    width: 400px;
-    margin-top: 50px;
-    left: 50%;
-    margin-left: -200px;
-    position: absolute;
+// Common
+const BakhwaWallLine = styled.div`
+    width: 100%;
+    height: 40px;
+    background-color: ${webColor.colors.bakhwa.dark};
+    font-family: 'Nanum Myeongjo', serif;
+    color: white;
+    font-size: 25px;
+    padding-top: 10px;
+    margin-top: 10px;
+    padding-left: 20px;
 `
-const MainLayoutInPC = styled.div`
 
-    margin-top: 300px;
-    margin-left: 30px;
-    margin-right: 30px;
-    width: 90%;
+const CobaltoWallLine = styled.div`
+    width: 100%;
+    height: 40px;
+    background-color: ${webColor.colors.cobalto.dark};
+    font-family: 'Android_101';
+    color: white;
+    font-size: 25px;
+    padding-top: 10px;
+    margin-top: 30px;
+    padding-left: 20px;
+`
 
-    position: absolute;
+const CommunicationWallLine = styled.div`
+    width: 100%;
+    height: 40px;
+    background-color: ${webColor.colors.box.main};
+    font-family: 'Android_101';
+    color: white;
+    font-size: 25px;
+    padding-top: 10px;
+    margin-top: 30px;
+    padding-left: 20px;
+    text-align: center;
+`
+
+// Only PC
+const MainDisplayInPC = styled.img`
+    width: 100%;
+    margin-top: 2px;
+`
+
+const BakhwaNewsLayoutInPC = styled.div`
+    width: 1200px;
+    height: 600px;
+    background-color: ${webColor.colors.bakhwa.flated};
+    margin-top: 30px;
+    margin-left: 80px;
+`
+const BakhwaNewsTitleText = styled.h1`
+    color: ${webColor.colors.bakhwa.dark};
+    padding-top: 10px;
+    padding-left: 20px;
+    font-family: 'Nanum Myeongjo', serif;
+`
+const BakhwaNewsImg = styled.img`
+    width: 1010px;
+    margin-left: 20px;
+    box-shadow: 3px 3px 3px black;
+
+`
+
+const CobaltoNewsLayoutInPC = styled.div`
+    width: 1200px;
+    height: 600px;
+    background-color: ${webColor.colors.cobalto.flated};
+    margin-top: 30px;
+    margin-left: 80px;
+`
+const CobaltoNewsTitleText = styled.h1`
+    color: ${webColor.colors.cobalto.dark};
+    padding-top: 10px;
+    padding-left: 20px;
+    font-family: 'Android_101';
+`
+const CobaltoNewsImg = styled.img`
+    width: 1010px;
+    margin-left: 20px;
+    box-shadow: 3px 3px 3px black;
+
 `
 // MOBILE EDITION
-const TitleImgInMobile = styled.img`
-    width: 300px;
-    margin-top: 30px;
-    left: 50%;
-    margin-left: -150px;
-    position: absolute;
-`
-
-const MainLayoutInMobile = styled.div`
-    margin-top: 200px;
-    margin-left: 30px;
-    margin-right: 30px;
-    width: 90%;
-
-    position: absolute;
-`
 
 export default PageHome;
